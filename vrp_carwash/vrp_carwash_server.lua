f45407adda439bd78ccc88a744171a7c14c80896
Pedro Silva <sparta14gaming@gmail.com>
2018-02-17 16:36:49 +0000

    Update vrp_carwash_server.lua
    moved everything vrp related inside "if enableprice == true" so it is able to still work for free without vrp

--- vrp_carwash/vrp_carwash_server.lua
+++ vrp_carwash/vrp_carwash_server.lua
@@ -1,31 +1,32 @@
 --Settings--
 
-enableprice = true -- true = carwash is paid, false = carwash is free
+enableprice = false -- true = carwash is paid, false = carwash is free
 
 price = 100 -- you may edit this to your liking. if "enableprice = false" ignore this one
 
 --DO-NOT-EDIT-BELLOW-THIS-LINE--
 
-local Tunnel = module("vrp", "lib/Tunnel")
-local Proxy = module("vrp", "lib/Proxy")
-
-vRP = Proxy.getInterface("vRP")
-
-vRPclient = Tunnel.getInterface("vRP", "vrp_carwash")
-
 RegisterServerEvent('vrp_carwash:checkmoney')
 AddEventHandler('vrp_carwash:checkmoney', function ()
-	user = vRP.getUserId({source})
-	_source = vRP.getUserSource({user})
 	if enableprice == true then
+		local Tunnel = module("vrp", "lib/Tunnel")
+		local Proxy = module("vrp", "lib/Proxy")
+
+		vRP = Proxy.getInterface("vRP")
+
+		vRPclient = Tunnel.getInterface("vRP", "vrp_carwash")
+		
+		user = vRP.getUserId({source})
+		
 		userMoney = vRP.getMoney({user})
 		if vRP.tryPayment({user, price}) then
-			TriggerClientEvent('vrp_carwash:success', _source, price)
+			TriggerClientEvent('vrp_carwash:success', source, price)
 		else
 			moneyleft = price - userMoney
-			TriggerClientEvent('vrp_carwash:notenoughmoney', _source, moneyleft)
+			TriggerClientEvent('vrp_carwash:notenoughmoney', source, moneyleft)
 		end
 	else
-		TriggerClientEvent('vrp_carwash:free', _source)
+		TriggerClientEvent('vrp_carwash:free', source)
 	end
 end)
+
