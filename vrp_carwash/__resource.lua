7a6c7422d85808fe7d2fb69d202f81213b1ade0c
Pedro Silva <sparta14gaming@gmail.com>
2018-02-17 16:35:47 +0000

    Update and rename _resource.lua to __resource.lua
    Fixed Unable to load vrp_carwash
    deleted dependency so it is able to run without vrp

--- vrp_carwash/__resource.lua
+++ vrp_carwash/__resource.lua
@@ -2,14 +2,12 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'vRP CarWash by TheSpartaPT.'
 
-dependency 'vrp'
-
 client_scripts {
-	'@vrp/client/Proxy.lua',
-	'vrp_carwash_client.lua'
+	"@vrp/client/Proxy.lua",
+	"vrp_carwash_client.lua"
 }
 
 server_scripts {
-	'@vrp/lib/utils.lua',
-	'vrp_carwash_server.lua'
+	"@vrp/lib/utils.lua",
+	"vrp_carwash_server.lua"
 }
